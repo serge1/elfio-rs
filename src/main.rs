356e04d38ad5ccda24564c6a921ffb3fd65d6c17
Serge Lamikhov-Center <to_serge@hotmail.com>
2021-04-16 11:49:36 +0300

    Create a separate elfio.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,155 +1,9 @@
 mod types;
 pub use types::*;
 
-mod elfio {
-    use std::fs::File;
-    use std::io;
-    use std::io::prelude::*;
-    use std::slice;
+mod elfio;
+pub use crate::elfio::*;
 
-    // Identification index
-    pub const EI_MAG0: usize = 0;
-    pub const EI_MAG1: usize = 1;
-    pub const EI_MAG2: usize = 2;
-    pub const EI_MAG3: usize = 3;
-    pub const EI_CLASS: usize = 4;
-    pub const EI_DATA: usize = 5;
-    pub const EI_VERSION: usize = 6;
-    pub const EI_OSABI: usize = 7;
-    pub const EI_ABIVERSION: usize = 8;
-    pub const EI_PAD: usize = 9;
-    pub const EI_NIDENT: usize = 16;
-
-    // Magic number
-    pub const ELFMAG0: u8 = 0x7F;
-    pub const ELFMAG1: u8 = 'E' as u8;
-    pub const ELFMAG2: u8 = 'L' as u8;
-    pub const ELFMAG3: u8 = 'F' as u8;
-
-    // File class
-    pub const ELFCLASSNONE: u8 = 0;
-    pub const ELFCLASS32: u8 = 1;
-    pub const ELFCLASS64: u8 = 2;
-
-    // Encoding
-    pub const ELFDATANONE: u8 = 0;
-    pub const ELFDATA2LSB: u8 = 1;
-    pub const ELFDATA2MSB: u8 = 2;
-
-    // ELF file header
-    #[repr(C)]
-    #[derive(Debug)]
-    struct ElfEhdr<Addr, Offset> {
-        e_ident: [u8; EI_NIDENT],
-        e_type: crate::types::ElfHalf,
-        e_machine: crate::types::ElfHalf,
-        e_version: crate::types::ElfWord,
-        e_entry: Addr,
-        e_phoff: Offset,
-        e_shoff: Offset,
-        e_flags: crate::types::ElfWord,
-        e_ehsize: crate::types::ElfHalf,
-        e_phentsize: crate::types::ElfHalf,
-        e_phnum: crate::types::ElfHalf,
-        e_shentsize: crate::types::ElfHalf,
-        e_shnum: crate::types::ElfHalf,
-        e_shstrndx: crate::types::ElfHalf,
-    }
-
-    trait ElfHeaderTrait {
-        fn load(&self) -> Result<(), &'static str>;
-    }
-
-    enum ElfHeader {
-        ElfHeader32(ElfEhdr<crate::types::Elf32Addr, crate::types::Elf32Off>),
-        ElfHeader64(ElfEhdr<crate::types::Elf64Addr, crate::types::Elf64Off>),
-    }
-
-    impl ElfHeaderTrait for ElfHeader {
-        fn load(&self) -> Result<(), &'static str> {
-            println!("'load' has been called");
-
-            match &self {
-                ElfHeader::ElfHeader64(_) => println!("64-bit header load"),
-                ElfHeader::ElfHeader32(_) => println!("32-bit header load"),
-            }
-
-            return Ok(());
-        }
-    }
-
-    pub struct Elfio {
-        header: Option<Box<dyn ElfHeaderTrait>>,
-    }
-
-    impl Elfio {
-        pub fn new() -> Elfio {
-            return Elfio { header: None };
-        }
-
-        fn read_struct<T, R: Read>(buffer: &mut R) -> io::Result<T> {
-            let num_bytes = ::std::mem::size_of::<T>();
-            unsafe {
-                let mut mem = ::std::mem::MaybeUninit::uninit().assume_init();
-                let ptr = slice::from_raw_parts_mut(&mut mem as *mut T as *mut u8, num_bytes);
-                match buffer.read_exact(ptr) {
-                    Ok(()) => Ok(mem),
-                    Err(e) => {
-                        ::std::mem::forget(mem);
-                        Err(e)
-                    }
-                }
-            }
-        }
-
-        fn create_and_load_header(&mut self, buffer: &mut File, class: u8) -> io::Result<()> {
-            if class == ELFCLASS64 {
-                let header = Self::read_struct::<ElfEhdr<crate::types::Elf64Addr, crate::types::Elf64Off>, File>(buffer)?;
-                self.header = Some(Box::new(ElfHeader::ElfHeader64(header)));
-            } else {
-                let header = Self::read_struct::<ElfEhdr<crate::types::Elf32Addr, crate::types::Elf32Off>, File>(buffer)?;
-                self.header = Some(Box::new(ElfHeader::ElfHeader32(header)));
-            }
-
-            Ok(())
-        }
-
-        pub fn load(&mut self, mut buffer: File) -> io::Result<()> {
-            let mut e_ident: [u8; EI_NIDENT] = [0; EI_NIDENT];
-            // Read ELF file signature
-            buffer.read_exact(&mut e_ident)?;
-            buffer.seek(io::SeekFrom::Start(0))?;
-
-            // Is it ELF file?
-            if e_ident[EI_MAG0] != ELFMAG0
-                || e_ident[EI_MAG1] != ELFMAG1
-                || e_ident[EI_MAG2] != ELFMAG2
-                || e_ident[EI_MAG3] != ELFMAG3
-            {
-                return Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    "File signature doesn't conform ELF file",
-                ));
-            }
-
-            if e_ident[EI_CLASS] != ELFCLASS64 && e_ident[EI_CLASS] != ELFCLASS32 {
-                return Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    "Unknown ELF class value",
-                ));
-            }
-
-            if e_ident[EI_DATA] != ELFDATA2LSB && e_ident[EI_DATA] != ELFDATA2MSB {
-                return Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    "Unknown ELF file endianess",
-                ));
-            }
-
-            self.create_and_load_header(&mut buffer, e_ident[EI_CLASS])
-        }
-    }
-}
 
 use std::io;
 
@@ -160,9 +14,9 @@ fn main() -> io::Result<()> {
     //    let mut file = File::open("/home/user/ELFIO/tests/elf_examples/hello_32")?;
     let file = File::open("/home/user/elfio-rs/target/debug/elfio-rs")?;
 
-    let mut elfio = elfio::Elfio::new();
+    let mut reader = elfio::Elfio::new();
 
-    elfio.load(file)?;
+    reader.load(file)?;
 
     Ok(())
 }
